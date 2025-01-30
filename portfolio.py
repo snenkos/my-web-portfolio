60d610c5f879ac890904064cb5e8192e4003031d
snenkos <167149881+snenkos@users.noreply.github.com>
2025-01-30 15:06:03 +0200

    Initial commit

---
+++ portfolio.py
@@ -0,0 +1,28 @@
+import streamlit as st
+
+st.write("""
+## Sinenkosi Sikhakhane - Data Scientist
+
+**Skills:**
+- Data Analysis
+- Machine Learning
+- MLOps
+- App Development
+- Power BI
+- SQL
+- Python
+- Data Visualization
+- Time Management
+- Problem-Solving
+- Collaboration
+- Adaptability
+- Effective Communication
+
+**About Me:**
+Enthusiastic and accomplished Data Scientist with a passion for harnessing advanced technologies to drive efficient data integration and analytics. I hold a Bachelor's Degree in Sport Science and a Certificate in Data Science, achieved with distinction. My experience spans implementing machine learning models and designing recommendation systems to deliver actionable insights. A notable achievement includes surpassing 70% accuracy in predicting customer satisfaction, showcasing my ability to transform complex data into meaningful outcomes. I excel in collaborating with cross-functional teams and am continuously eager to tackle new challenges. With a solid grasp of ERP software and proficiency in Python, SQL, and Power BI, I bring a well-rounded skill set to every project.
+
+**Education:**
+- 2017 - 2020: **Tshwane University of Technology** - Sport Science
+- 2024 - 2025: **Explore AI Academy** - Data Science
+""")
+
